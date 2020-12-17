808f800df0fd484e0a313ab502faf6566dab2474
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-18 00:02:34 +0300

    problem has finished

--- app/src/main/java/com/kumsal/fab_learning/MainActivity.kt
+++ app/src/main/java/com/kumsal/fab_learning/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kumsal.fab_learning
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.OvershootInterpolator
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -11,9 +12,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var fab2: FloatingActionButton
     private lateinit var fab3: FloatingActionButton
     var isOpen:Boolean=false
+    var interPolator:OvershootInterpolator= OvershootInterpolator()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initial()
+
+    }
+
+    private fun initial() {
         fab = findViewById(R.id.fab)
         fab1 = findViewById(R.id.fab1)
         fab2 = findViewById(R.id.fab2)
@@ -23,15 +31,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         fab3.alpha = 0F
         fab1.alpha = 0F
 
-        fab3.translationY=100F
-        fab2.translationY=100F
-        fab1.translationY=100F
+        fab3.translationY = 100F
+        fab2.translationY = 100F
+        fab1.translationY = 100F
 
         fab.setOnClickListener(this)
         fab1.setOnClickListener(this)
         fab2.setOnClickListener(this)
         fab3.setOnClickListener(this)
+    }
+    private fun open(){
+        isOpen=true
+        fab.animate().setInterpolator(interPolator).rotationBy(45f).setDuration(300).start()
+        fab1.animate().translationY(0F).alpha(1F).setInterpolator(interPolator).setDuration(300).start()
+        fab2.animate().translationY(0F).alpha(1F).setInterpolator(interPolator).setDuration(300).start()
+        fab3.animate().translationY(0F).alpha(1F).setInterpolator(interPolator).setDuration(300).start()
 
+    }
+    private fun close(){
+        isOpen=false
+        fab.animate().setInterpolator(interPolator).rotationBy(0f).setDuration(300).start()
+        fab1.animate().translationY(100F).alpha(0F).setInterpolator(interPolator).setDuration(300).start()
+        fab2.animate().translationY(100F).alpha(0F).setInterpolator(interPolator).setDuration(300).start()
+        fab3.animate().translationY(100F).alpha(0F).setInterpolator(interPolator).setDuration(300).start()
 
     }
 
